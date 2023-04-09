75ff932ec1d6e815b20c2136cf9497b5bc97e371
Sabah Nushra <86923009+Meowthhh@users.noreply.github.com>
2023-04-09 13:31:02 +0600

    Add files via upload

---
+++ function.dart
@@ -0,0 +1,17 @@
+void main()
+{
+    print('something');
+    String greet = greetings();
+    int age = GetAge();
+    print(age);
+    print(greet);
+}
+
+String greetings()
+{
+    return "Hello";
+}
+int GetAge()
+{
+    return 20;
+}
